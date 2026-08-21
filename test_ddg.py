1e34e5f2875871d14d59bf4ff297429fc8397c98
VarshneysvAI <varshneyshourya19@gmail.com>
2026-08-21 17:45:09 +0530

    chore: remove test script

--- test_ddg.py
+++
@@ -1,6 +0,0 @@
-import httpx
-from bs4 import BeautifulSoup
-res = httpx.post('https://html.duckduckgo.com/html/', data={'q':'Appliance Dealers Cooperative PDSH4816AF specification'}, headers={'User-Agent': 'Mozilla/5.0'})
-soup = BeautifulSoup(res.text, 'html.parser')
-for a in soup.select('.result__a'):
-    print(a['href'])
